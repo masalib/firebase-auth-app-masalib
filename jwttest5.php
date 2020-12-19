9e37322c8a409e33952beccfe3fb0ae7412a2c8a
masalib <masalib@gmail.com>
2020-12-19 15:39:01 +0900

    CORSの対応を追加

--- jwttest5.php
+++ jwttest5.php
@@ -3,6 +3,27 @@ require_once "vendor/autoload.php";
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+$origin = "";
+$method = "";
+$headers = "";
+
+if(isset($_SERVER['HTTP_ORIGIN'])) {
+    $origin = $_SERVER['HTTP_ORIGIN'];
+}
+if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+    $method = $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'];
+}
+if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+    $headers = $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'];
+}
+
+/* ここに$origin、$method、$headersをチェックする処理を入れる */
+
+header('Access-Control-Allow-Origin: ' . $origin);
+header('Access-Control-Allow-Method: ' . $method);
+header('Access-Control-Allow-Headers: ' . $headers);
+
+
 
 use \Firebase\JWT\JWT;
 $headers = apache_request_headers();
